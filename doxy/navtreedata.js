f7994a63d3cbd58202771aae735ffde7bccdbddf
henrygab <henrygab@users.noreply.github.com>
2024-12-30 19:12:53 +0000

    deploy: 26550c060e21a37371a38d00aca941c4e560b853

---
+++ doxy/navtreedata.js
@@ -0,0 +1,74 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ESPixelStick Firmware", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_const_names_8cpp.html",
+"_const_names_8hpp.html#a0e21d787232558369b5d86e747610ce0",
+"_e_s_pixel_stick_8h.html#a43e1cad902b6477bec893cb6430bd6c8",
+"_g_p_i_o___defs___e_s_p32___d1___m_i_n_i___e_t_h_8hpp.html#af2936ca829929ac3e76f0969fdfbf732",
+"_g_p_i_o___defs___e_s_p32___octa2go_8hpp.html#aff0e68ebc94664f2126bfe4da79be58e",
+"_g_p_i_o___defs___e_s_p32___q_u_i_n_l_e_d___u_n_o___a_e___plus_8hpp_source.html",
+"_g_p_i_o___defs___e_s_p32___w_t32_e_t_h01_8hpp.html#aeaea1905632a9d6e4909b54c29fb6d56",
+"_output_g_e_c_e_8hpp_source.html",
+"classc___f_p_p_discovery.html",
+"classc___input_e131.html#a80b20934b5e5ac22dd3a019cf51a4267",
+"classc___input_m_q_t_t.html#a73ae0910624d8445acede0cddf55664e",
+"classc___output_pixel.html#a77f8540f4181af47f5c3fc119ab21d01",
+"classfsm___play_effect__state___idle.html#a61b7c76b8f48a9bb5ff988131d133d9f",
+"fseq_8h.html#a61f3ec214a6fa2bb351949c606ed87b9",
+"struct_f_s_e_q_raw_variable_data_header.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
